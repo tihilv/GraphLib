b7a9ee9b89f5255a8e7d38b36d6304cbfd7c5fe3
Ilia Tikhonov <tihilv@gmail.com>
2017-07-13 13:19:46 +0300

    Heuristic TSP Finder

---
+++ GraphLib/Tsp/HeuristicTspFinder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphLib.Tsp
+{
+    public class HeuristicTspFinder
+    {
+        private readonly List<TspPoint> _points;
+
+        public HeuristicTspFinder(List<TspPoint> points)
+        {
+            _points = points;
+        }
+
+        public double Process()
+        {
+            var firstVertex = _points[0];
+            HashSet<TspPoint> visitedVertices = new HashSet<TspPoint>();
+
+            double distance = 0;
+
+            var currentVertex = firstVertex;
+            for (int i = 0; i < _points.Count-1; i++)
+            {
+                visitedVertices.Add(currentVertex);
+
+                var closestVertex = _points.Where(v => !visitedVertices.Contains(v)).OrderBy(v => v.DistanceToSquared(currentVertex)).First();
+
+                distance += closestVertex.DistanceTo(currentVertex);
+                currentVertex = closestVertex;
+            }
+
+            distance += currentVertex.DistanceTo(firstVertex);
+
+            return distance;
+        }
+    }
+
+    public struct TspPoint
+    {
+        public readonly double X;
+        public readonly double Y;
+
+        public TspPoint(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double DistanceTo(TspPoint p)
+        {
+            return Math.Sqrt(DistanceToSquared(p));
+        }
+
+        public double DistanceToSquared(TspPoint p)
+        {
+            return Math.Pow(X - p.X, 2) + Math.Pow(Y - p.Y, 2);
+        }
+    }
+}
+
--- GraphLibTests/TspTests.cs
+++ GraphLibTests/TspTests.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Reflection;
 using System.Text;
 using GraphLib;
-using GraphLib.SccDetection;
 using GraphLib.Tsp;
 using Xunit;
 
@@ -16,11 +15,46 @@ namespace GraphLibTests
         [Fact]
         public void TestFromFile()
         {
+            var graph = GetGraph("tsp1.txt", TspFinder.OptimizedOptions());
+
+            TspFinder finder = new TspFinder(graph);
+            var result = finder.Process();
+            Assert.Equal(true, Math.Abs(4.41-result) < 0.01);
+        }
+
+        [Fact]
+        public void HeuristicTestFromFile()
+        {
+            var graph = GetTspPoints("tsp_big.txt");
+
+            HeuristicTspFinder finder = new HeuristicTspFinder(graph);
+            var result = finder.Process();
+            Assert.Equal(true, Math.Abs(1203406.5012708856 - result) < 0.01);
+        }
+
+
+        private Graph GetGraph(string fileName, GraphOptions options)
+        {
+            var points = GetTspPoints(fileName);
+
+            Graph graph = new Graph(options);
+            for (int i = 0; i < points.Count; i++)
+                for (int j = i + 1; j < points.Count; j++)
+                {
+                    var dist = points[i].DistanceTo(points[j]);
+                    graph.AddEdge(i.ToString(), j.ToString(), dist);
+                }
+
+            return graph;
+        }
+
+        private static List<TspPoint> GetTspPoints(string fileName)
+        {
             var location = Path.GetDirectoryName(typeof(TspTests).GetTypeInfo().Assembly.Location);
-            var path = Path.Combine(location, "..", "..", "..", "data", "tsp1.txt");
+            var path = Path.Combine(location, "..", "..", "..", "data", fileName);
 
 
-            List<Point> points = new List<Point>();
+            List<TspPoint> points = new List<TspPoint>();
 
             using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
             using (var textReader = new StreamReader(stream, Encoding.ASCII))
@@ -33,40 +67,10 @@ namespace GraphLibTests
                     var x = double.Parse(str.Substring(0, index).Replace(".", CultureInfo.CurrentCulture.NumberFormat.CurrencyDecimalSeparator));
                     var y = double.Parse(str.Substring(index + 1).Replace(".", CultureInfo.CurrentCulture.NumberFormat.CurrencyDecimalSeparator));
 
-                    points.Add(new Point(x, y));
+                    points.Add(new TspPoint(x, y));
                 }
             }
-
-            Graph graph = new Graph(TspFinder.OptimizedOptions());
-            for (int i = 0; i < points.Count; i++)
-            for (int j = i + 1; j < points.Count; j++)
-            {
-                var dist = points[i].DistanceTo(points[j]);
-                graph.AddEdge(i.ToString(), j.ToString(), dist);
-            }
-
-            TspFinder finder = new TspFinder(graph);
-            var result = finder.Process();
-            Assert.Equal(true, Math.Abs(4.41-result) < 0.01);
+            return points;
         }
-
-
-        struct Point
-        {
-            public readonly double X;
-            public readonly double Y;
-
-            public Point(double x, double y)
-            {
-                X = x;
-                Y = y;
-            }
-
-            public double DistanceTo(Point p)
-            {
-                return Math.Sqrt(Math.Pow(X - p.X, 2) + Math.Pow(Y - p.Y, 2));
-            }
-        }
-
     }
 }
